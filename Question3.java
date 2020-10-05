8d486dc61227e514d46f43db6f6b7119f092894b
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-05 11:12:15 -0400

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -1,11 +1,20 @@
 /* Activity 1.2 - Question 3 */
 
-
+import java.util.Scanner;
 
 class Question3 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+        Scanner scan = new Scanner(System.in);
+        String name;
+        String adj;
+
+        System.out.println("What is your name?");
+        name = scan.nextLine();
+        System.out.println("Whats a word that describes you?");
+        adj = scan.nextLine();
+        
+        System.out.println("My name is " + name + ". I am " + adj + ".");
 
     }
 }
