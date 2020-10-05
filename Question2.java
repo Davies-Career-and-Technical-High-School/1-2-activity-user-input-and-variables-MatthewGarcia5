640981fe0eead75adac3949b7e59512cce91b641
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-05 11:09:05 -0400

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,11 +1,24 @@
 /* Activity 1.2 - Question 2 */
 
-
+import java.util.Scanner;
 
 class Question2 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+        Scanner scan = new Scanner(System.in);
+        String name;
+        String name2;
+        String name3;
 
+        System.out.println("Put in a name");
+        name = scan.nextLine();
+        System.out.println("Do it again");
+        name2 = scan.nextLine();
+        System.out.println("Do it again");
+        name3 = scan.nextLine();
+
+        System.out.println(name3);
+        System.out.println(name2);
+        System.out.println(name);
     }
 }
